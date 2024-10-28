c51f2dec14cbb01061a21bd3b85dbf26caf77702
Samuel Rüegger <samuel@rueegger.me>
2024-10-28 13:24:49 +0100

    make string translatable

--- inc/functions.php
+++ inc/functions.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* Add Swiss Phone Format to GravityForms Phone Field */
 function spsgf_ch_phone_format( $phone_formats ) {
   $phone_formats['uk'] = array(
-    'label'       => 'Switzerland',
+    'label'       => __( 'Switzerland', 'swissphone-select-for-gravityforms' ),
     'mask'        => '+41 99 999 99 99',
     'regex'       => '/^(?:\+41|0041|0)?\s?(?:\d{2}\s?\d{3}\s?\d{2}\s?\d{2})$/',
     'instruction' => '+41 79 000 00 00',
